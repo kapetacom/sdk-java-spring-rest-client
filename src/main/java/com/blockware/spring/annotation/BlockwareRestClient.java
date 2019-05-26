07b5c1ed132d225d08c88b1400d49b8dddc76a06
Henrik von Hofmeister <hh@cloudscrape.com>
2019-05-26 19:35:11 +0200

    Make clients components

--- src/main/java/com/blockware/spring/annotation/BlockwareRestClient.java
+++ src/main/java/com/blockware/spring/annotation/BlockwareRestClient.java
@@ -1,5 +1,13 @@
 package com.blockware.spring.annotation;
 
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Component
 public @interface BlockwareRestClient {
 
     String value();
