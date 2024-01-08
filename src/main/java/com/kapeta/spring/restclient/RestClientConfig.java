386a7be3ee48c3caf1909ad85661354a35deaedd
Henrik Hofmeister <henrik@newdawn.dk>
2024-01-08 20:52:22 +0100

    feat: Add auth forwarder interception
    Also adds some standard headers to all requests

--- src/main/java/com/kapeta/spring/restclient/RestClientConfig.java
+++ src/main/java/com/kapeta/spring/restclient/RestClientConfig.java
@@ -6,12 +6,16 @@
 package com.kapeta.spring.restclient;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kapeta.spring.config.providers.KapetaConfigurationProvider;
+import com.kapeta.spring.security.AuthorizationForwarderSupplier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 public class RestClientConfig {
 
     @Bean
-    public RestClientFactorySupport restClientFactorySupport() {
-        return new RestClientFactorySupport();
+    public RestClientFactorySupport restClientFactorySupport(ObjectMapper objectMapper, Environment environment, KapetaConfigurationProvider configurationProvider, AuthorizationForwarderSupplier authorizationForwarder) {
+        return new RestClientFactorySupport(objectMapper, environment, configurationProvider, authorizationForwarder.get());
     }
 }
